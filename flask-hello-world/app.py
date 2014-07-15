5174cdef3bc95496dbe109fd3645ae6b75d89c0d
colorsocks <babkin@colorsocks.ru>
2014-07-15 22:39:14 +0400

    debugger and d route

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,9 +6,17 @@ from flask import Flask
 # create the oplication object 
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to links the function to a url 
 @app.route("/")
 @app.route("/hello")
+
+# dinamic routes
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
 
 # define the view use the function, which returns the string
 def HelloWorld():
